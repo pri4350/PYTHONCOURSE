6f06fe20a6f09bedb3a0d8f5f5cd8c5cda18fd73
Priyanka bhole <priyankabhole21@gmail.com>
2025-12-30 18:34:52 +0530

    change

--- Chapter8.py
+++ Chapter8.py
@@ -7,37 +7,33 @@ with open('intro.txt', 'w') as intro_file:
 
 #create a file goals.txt and write 3 goals for this year.
 
-# with open('goals.txt', 'w') as goals_file:
-#     goals_file.write("1. Improve my math skills.\n")
-#     goals_file.write("2. Read at least 10 books.\n")
-#     goals_file.write("3. Participate in a school club.\n")
+with open('goals.txt', 'w') as goals_file:
+    goals_file.write("1. Improve my math skills.\n")
+    goals_file.write("2. Read at least 10 books.\n")
+    goals_file.write("3. Participate in a school club.\n")
 
 #Append "completed" to an existing file status.txt.
 with open('status.txt', 'a') as status_file:
     status_file.write("completed\n")
 
-
-
-
-
 #copy notes.txt to notes_backup.txt
-# with open('notes.txt', 'r') as notes_file:
-#     notes_content = notes_file.read()
-# with open('notes_backup.txt', 'w') as backup_file:
-#     backup_file.write(notes_content)
+with open('notes.txt', 'r') as notes_file:
+    notes_content = notes_file.read()
+with open('notes_backup.txt', 'w') as backup_file:
+    backup_file.write(notes_content)
 
 #rename temp.txt to final.txt
-# import os
-# os.rename('temp.txt', 'final.txt')
+import os
+os.rename('temp.txt', 'final.txt')
 
 #Ask user for a filename and copy it to a backup folder.
-# import os
-# filename = input("Enter the filename to back up: ")
-# backup_folder = 'backup'
-# if not os.path.exists(backup_folder):
-#     os.makedirs(backup_folder)
-# with open(filename, 'r') as original_file:
-#     content = original_file.read()
-# with open(os.path.join(backup_folder, filename), 'w') as backup_file:
-#     backup_file.write(content)
+import os
+filename = input("Enter the filename to back up: ")
+backup_folder = 'backup'
+if not os.path.exists(backup_folder):
+    os.makedirs(backup_folder)
+with open(filename, 'r') as original_file:
+    content = original_file.read()
+with open(os.path.join(backup_folder, filename), 'w') as backup_file:
+    backup_file.write(content)
     
